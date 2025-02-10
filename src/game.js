3216a9774f52f5f0ce79b3e5ad0d608d8394cbb8
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-02-10 12:11:19 +0700

    add askquestion file

--- src/game.js
+++ src/game.js
@@ -1,27 +1,37 @@
-const { AskQuestions } = require("./main");
+const db = require('./database');
+const { AskQuestions, rl } = require('./askquestion')
 
-const game = async(chances) => {
+const game = async(chances, difficult) => {
     let value = Math.floor(Math.random() * 100) + 1;
     let guess;
 
     while(chances !== 0) {
-        guess = parseInt(AskQuestions('Enter your guess: '));
+        guess = parseInt(await AskQuestions('Enter your guess: '));
 
         if(guess === value) {
             console.log(`Congratulations! You guessed the correct number in ${chances} attempts.\n`);
             
-            const [result] = await db.query
+            const [result] = await db.query(`SELECT * FROM highscore WHERE difficult = ?`, [difficult]);
+            
+            if(chances > result[0].score) {
+                console.log(`Congratulations you break the records of the difficult ${difficult}\n`);
+                await db.query(`UPDATE highscore SET score = ? WHERE difficult = ?`, [chances, difficult]);
+            };
             break;
         } else if(guess > value) {
-            console.log(`Incorrect! The number is greater than ${guess}.\n`);
+            console.log(`Incorrect! The number is less than ${guess}.\n`);
             chances--;
         } else {
-            console.log(`Incorrect! The number is less than ${guess}.\n`);
+            console.log(`Incorrect! The number is greater than ${guess}.\n`);
             chances--;
         }; 
     };
+
+    if(chances === 0) {
+        console.log(`Game over, you lose`);
+    };
 };
 
-module.exports = game
+module.exports = game;
 
 
--- src/main.js
+++ src/main.js
@@ -1,50 +1,40 @@
 const game = require("./game");
 const gameMessage = require("./message");
-const readline = require('readline');
-
-const AskQuestions = (query) => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    return new Promise(resolve => {
-        rl.question(query, (answer) => {
-            rl.close();
-            resolve(answer);
-        })
-    })
-};
+const { AskQuestions, rl } = require('./askquestion');
 
 const main = async() => {
     gameMessage();
     let play = true;
     let chances;
+    let difficult;
 
     while(play) {
         console.log("Please select the difficult level:");
         console.log("1. Easy(10 chances)\n2. Medium (5 chances)\n3. Hard(3 chances)\n");
     
-        const difficult = await AskQuestions("Enter your choice: ")
+        let difficult_number = await AskQuestions("Enter your choice: ")
 
-        switch(difficult.toLowerCase()) {
-            case 'easy':
+        switch(difficult_number) {
+            case '1':
                 chances = 10;
+                difficult = 'Easy';
                 break;
-            case 'medium':
+            case '2':
                 chances = 5;
+                difficult = 'Medium';
                 break;
-            case 'hard': 
+            case '3': 
                 chances = 3;
+                difficult = 'Hard';
                 break;
             default: 
-                console.log(`Input yang anda masukkan tidak valid\n`);
+                console.log(`Input yang anda masukkan tidak valid, coba lagi\n`);
                 continue;           
         };
 
         console.log(`Great! You have selected the ${difficult} difficulty level. Let's start the game!`);
 
-        game(chances, difficult);
+        await game(chances, difficult);
 
         const playAgain = await AskQuestions('Do you want to play again? (yes/no): ');
         play = playAgain.toLowerCase() === 'yes';
@@ -54,6 +44,6 @@ const main = async() => {
 
 main();
 
-module.exports = { AskQuestions };
+module.exports = AskQuestions;
 
 
